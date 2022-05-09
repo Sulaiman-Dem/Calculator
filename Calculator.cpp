c3abb48b2bd62849a0f205de6a27a4621aec8a9a
Dremsara <winged.demsar2@gmail.com>
2022-05-09 16:10:06 -0400

    practice

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main()
+long double addition(long double num1, long double num2)
 {
 
+    return num1 + num2;
+}
+
+int main()
+{
+    long double num2;
+    long double num1;
+
+    cout << "Input first number: ";
+    cin >> num1;
+
+    cout << "Input second number: ";
+    cin >> num2;
+
+    addition(num1, num2);
     return 0;
 }
